fb0d805df26174fe36a734dd6e04f5b52ec048eb
youhei usami <usao9110@icloud.com>
2025-12-29 11:31:26 +0400

    12/29

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
 // "use client";
 
-type Post = {
-  id: number;
-  title: string;
-};
+import { Suspense } from "react";
+import SlowComponent from "./SlowComponent";
+
+// type Post = {
+//   id: number;
+//   title: string;
+// };
 
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts: Post[] = await res.json();
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const posts: Post[] = await res.json();
   return (
     <div>
-      <h1>記事一覧</h1>
+      <h1>メインコンテンツ(すぐに表示)</h1>
+      <Suspense fallback={<div>重いコンポーネントを読み込み中...</div>}>
+        <SlowComponent />
+      </Suspense>
+      {/* <h1>記事一覧</h1>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
