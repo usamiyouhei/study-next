345ee1bc99b17be0131b66474dc2c40692b6fb01
youhei usami <usao9110@icloud.com>
2025-12-29 12:26:22 +0400

    12/29

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,25 @@
-// "use client";
+"use client";
 
-import { Suspense } from "react";
-import SlowComponent from "./SlowComponent";
+import { FormEvent } from "react";
 
-// type Post = {
-//   id: number;
-//   title: string;
-// };
+export default function Home() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const name = form.get("name");
 
-export default async function Home() {
-  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  // const posts: Post[] = await res.json();
+    await fetch("/api/create", {
+      method: "POST",
+      headers: {
+        "Conyent-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+  };
   return (
-    <div>
-      <h1>メインコンテンツ(すぐに表示)</h1>
-      <Suspense fallback={<div>重いコンポーネントを読み込み中...</div>}>
-        <SlowComponent />
-      </Suspense>
-      {/* <h1>記事一覧</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul> */}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" />
+      <button type="submit">送信</button>
+    </form>
   );
 }
